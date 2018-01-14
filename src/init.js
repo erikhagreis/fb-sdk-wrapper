092fbd10e598d9d6fdc1e74367b2dc5ae649d2e1
Erik Hagreis <erik.hagreis@gmail.com>
2018-01-14 10:53:42 +0100

    fix: solve some syntax/babel issues

--- src/init.js
+++ src/init.js
@@ -1,21 +1,22 @@
+import { defaults } from 'lodash';
 import loadEnforcer from './loadEnforcer';
 
-
 /**
- * Used to initialize and setup the SDK. All other SDK methods must be called 
+ * Used to initialize and setup the SDK. All other SDK methods must be called
  * after this one.
- * 
+ *
  * https://developers.facebook.com/docs/javascript/reference/FB.init/v2.11
  * https://developers.facebook.com/apps/
- * 
+ *
  * @param {Object} FB - the window.FB object
  * @param {String} appId - the FB app ID
  * @param {Object} params - Params object (optional)
  */
-const init = loadEnforcer((FB, appId, params = { 
-  xbfml = false, 
-  version = 'v2.11' 
-}) => {
+const init = loadEnforcer((FB, appId, params = {}) => {
+  params = defaults({}, params, {
+    xbfml: false,
+    version: 'v2.11'
+  });
   FB.init({
     appId: appId,
     ...params
--- src/load.js
+++ src/load.js
@@ -1,28 +1,34 @@
+import { defaults } from 'lodash';
 
 /**
  * Injects the script for the FB JS SDK into the page.
- * 
+ *
  * @param {Object} params containing optional settings
- * 
+ *
  * @return {Promise} for the global window.FB object
  */
-export default function load(params = {
-  locale = 'en_US'
-}) {
+export default function load(params = {}) {
+  params = defaults({}, params, {
+    locale: 'en_US'
+  });
   return new Promise((resolve, reject) => {
     if (window.FB) {
       return resolve(window.FB);
     }
 
-    const src = `//connect.facebook.net/${params.locale}/sdk.js`
+    const src = `//connect.facebook.net/${params.locale}/sdk.js`;
     const script = document.createElement('script');
     script.id = 'facebook-jssdk';
     script.src = src;
     script.async = true;
     script.addEventListener('load', () => resolve(window.FB), false);
-    script.addEventListener('error', () => reject(`Error loading Facebook JS SDK from ${src}`), false);
+    script.addEventListener(
+      'error',
+      () => reject(`Error loading Facebook JS SDK from ${src}`),
+      false
+    );
 
     const sibling = document.getElementsByTagName('script')[0];
     sibling.parentNode.insertBefore(script, sibling);
   });
-};
+}
